7e2fa0060aaa3016e3d16d006754696d2c8eee84
Titos Gune <titosgune@gmail.com>
2013-07-19 15:16:50 +0200

    First Commit for Micro_app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MicroApp::Application.config.secret_token = '0952a29dde2b891bf25fa161823ef2e60c1df18a51b55ad708edc8f826d5d8d4723d2e4cafc181082506085ffe96f41eff67e5eee62c5cca9f776c5ed6488a40'
